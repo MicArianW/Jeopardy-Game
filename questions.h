5bff1229be0b5e840f5a4ac17b52426b5ab52606
Jarsith <166121419+Jarsith@users.noreply.github.com>
2025-02-11 20:25:31 -0500

    Update questions.h

--- questions.h
+++ questions.h
@@ -34,7 +34,7 @@ typedef struct {
 
 // An array of 12 questions (4 for each category), initialized in initialize_game
 // this may need to be a pointer if you want it set dynamically
-question questions[NUM_QUESTIONS];
+extern question questions[NUM_QUESTIONS];
 
 // Initializes the array of questions for the game
 extern void initialize_game(void);
@@ -50,5 +50,8 @@ extern bool valid_answer(char *category, int value, char *answer);
 
 // Returns true if the question has already been answered
 extern bool already_answered(char *category, int value);
+
+// Marks a question as answered
+extern void mark_question_answered(char *category, int value);
 
 #endif /* QUESTIONS_H_ */
